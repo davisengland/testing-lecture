56e8d28f72b84c4ad6b36ecbc0a58bc79297f17a
davisengland <davisrodneyengland@gmail.com>
2021-03-29 14:37:57 -0600

    Lecture 6.1

---
+++ src/functions.js
@@ -0,0 +1,20 @@
+const { default: Axios } = require("axios")
+
+module.exports = {
+    add: (num1, num2) => num1 + num2,
+
+    subtract: (num1, num2) => num1 - num2,
+
+    multiply: (num1, num2) => num1 * num2,
+
+    divide: (num1, num2) => num1 / num2,
+
+    sayHello: () => 'hello',
+
+    getUser: async () => {
+        const user = await axios.get('https://jsonplaceholder.typicode.com/users/1')
+            .catch(err => console.log(err))
+
+        return user.data
+    }
+}
